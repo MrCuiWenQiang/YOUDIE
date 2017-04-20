cb574db39cac59617d6b4dab67e00447de32afb0
TB <913552654@qq.com>
2017-04-20 10:18:54 +0800

    添加图片加载模块，具体下载部分还有待优化

---
+++ app/src/main/java/com/my/fakerti/util/loadimage/fragment/AbsSingleFragmentActivity.java
@@ -0,0 +1,33 @@
+package com.my.fakerti.util.loadimage.fragment;
+
+import android.os.Bundle;
+import com.my.fakerti.R;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public abstract class AbsSingleFragmentActivity extends FragmentActivity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(getLayoutId());
+
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.id_fragmentContainer);
+
+		if (fragment == null)
+		{
+			fragment = createFragment();
+			fm.beginTransaction().add(R.id.id_fragmentContainer, fragment)
+					.commit();
+		}
+
+	}
+
+	protected abstract Fragment createFragment();
+
+	protected abstract int getLayoutId();
+
+}
---
+++ app/src/main/java/com/my/fakerti/util/loadimage/fragment/ListImgsFragment.java
@@ -0,0 +1,81 @@
+package com.my.fakerti.util.loadimage.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.my.fakerti.R;
+import com.my.fakerti.util.loadimage.util.ImageLoader;
+import com.my.fakerti.util.loadimage.util.ImageLoader.Type;
+import com.my.fakerti.util.loadimage.util.Images;
+
+/**
+ * 三级缓冲异步加载图片例子
+ * mImageLoader.loadImage(getItem(position), imageview, true);
+ *
+ * @author TB
+ */
+public class ListImgsFragment extends Fragment {
+    private GridView mGridView;
+    private String[] mUrlStrs = Images.imageThumbUrls;
+    private ImageLoader mImageLoader;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mImageLoader = ImageLoader.getInstance(3, Type.LIFO);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_list_imgs, container,
+                false);
+        mGridView = (GridView) view.findViewById(R.id.id_gridview);
+        setUpAdapter();
+        return view;
+    }
+
+    private void setUpAdapter() {
+        if (getActivity() == null || mGridView == null)
+            return;
+
+        if (mUrlStrs != null) {
+            mGridView.setAdapter(new ListImgItemAdaper(getActivity(), 0,
+                    mUrlStrs));
+        } else {
+            mGridView.setAdapter(null);
+        }
+
+    }
+
+    private class ListImgItemAdaper extends ArrayAdapter<String> {
+
+        public ListImgItemAdaper(Context context, int resource, String[] datas) {
+            super(getActivity(), 0, datas);
+            Log.e("TAG", "ListImgItemAdaper");
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(
+                        R.layout.item_fragment_list_imgs, parent, false);
+            }
+            ImageView imageview = (ImageView) convertView
+                    .findViewById(R.id.id_img);
+            imageview.setImageResource(R.drawable.pictures_no);
+            mImageLoader.loadImage(getItem(position), imageview, true);
+            return convertView;
+        }
+
+    }
+
+}
